13a8fe30ca926ccd9e137340045a9f910bd4e547
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-01-01 22:42:06 +0300

    Add AssetEditorUtility

---
+++ Packages/UGF.EditorTools/Editor/Asset/AssetEditorUtility.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace UGF.EditorTools.Editor.Asset
+{
+    public class AssetEditorUtility
+    {
+        public static string GetAdditionalFilePath(string assetPath, string label, string extensionName)
+        {
+            if (string.IsNullOrEmpty(assetPath)) throw new ArgumentException("Value cannot be null or empty.", nameof(assetPath));
+            if (string.IsNullOrEmpty(label)) throw new ArgumentException("Value cannot be null or empty.", nameof(label));
+            if (string.IsNullOrEmpty(extensionName)) throw new ArgumentException("Value cannot be null or empty.", nameof(extensionName));
+
+            string directory = Path.GetDirectoryName(assetPath);
+            string name = Path.GetFileNameWithoutExtension(assetPath);
+
+            return $"{directory}/{name}.{label}.{extensionName}";
+        }
+    }
+}
