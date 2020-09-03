3edff913dc6c51be6b11ddcdb57bdfd04e36acc3
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-09-04 00:16:26 +0300

    Update DropdownGUIUtility.cs

--- Packages/UGF.EditorTools/Editor/IMGUI.Dropdown/DropdownGUIUtility.cs
+++ Packages/UGF.EditorTools/Editor/IMGUI.Dropdown/DropdownGUIUtility.cs
@@ -8,16 +8,24 @@ namespace UGF.EditorTools.Editor.IMGUI.Dropdown
     {
         public static bool DropdownButton(string content, out Rect dropdownPosition, FocusType focusType = FocusType.Keyboard, params GUILayoutOption[] options)
         {
+            if (content == null) throw new ArgumentNullException(nameof(content));
+
             return DropdownButton(GUIContent.none, new GUIContent(content), out dropdownPosition, focusType, options);
         }
 
         public static bool DropdownButton(string label, string content, out Rect dropdownPosition, FocusType focusType = FocusType.Keyboard, params GUILayoutOption[] options)
         {
+            if (label == null) throw new ArgumentNullException(nameof(label));
+            if (content == null) throw new ArgumentNullException(nameof(content));
+
             return DropdownButton(new GUIContent(label), new GUIContent(content), out dropdownPosition, focusType);
         }
 
         public static bool DropdownButton(GUIContent label, GUIContent content, out Rect dropdownPosition, FocusType focusType = FocusType.Keyboard, params GUILayoutOption[] options)
         {
+            if (label == null) throw new ArgumentNullException(nameof(label));
+            if (content == null) throw new ArgumentNullException(nameof(content));
+
             Rect position = EditorGUILayout.GetControlRect(label != GUIContent.none, options);
 
             return DropdownButton(position, label, content, out dropdownPosition, focusType);
@@ -25,17 +33,23 @@ namespace UGF.EditorTools.Editor.IMGUI.Dropdown
 
         public static bool DropdownButton(Rect position, string content, out Rect dropdownPosition, FocusType focusType = FocusType.Keyboard)
         {
+            if (content == null) throw new ArgumentNullException(nameof(content));
+
             return DropdownButton(position, GUIContent.none, new GUIContent(content), out dropdownPosition, focusType);
         }
 
         public static bool DropdownButton(Rect position, string label, string content, out Rect dropdownPosition, FocusType focusType = FocusType.Keyboard)
         {
+            if (label == null) throw new ArgumentNullException(nameof(label));
+            if (content == null) throw new ArgumentNullException(nameof(content));
+
             return DropdownButton(position, new GUIContent(label), new GUIContent(content), out dropdownPosition, focusType);
         }
 
         public static bool DropdownButton(Rect position, GUIContent label, GUIContent content, out Rect dropdownPosition, FocusType focusType = FocusType.Keyboard)
         {
             if (label == null) throw new ArgumentNullException(nameof(label));
+            if (content == null) throw new ArgumentNullException(nameof(content));
 
             if (label != GUIContent.none)
             {
