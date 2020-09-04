3d823d23d5c24c5d1c1c9758530499522288cde9
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-09-04 23:01:13 +0300

    Add DropdownTestPropertyDrawer

--- Assets/UGF.EditorTools.Editor.Tests/IMGUI.Dropdown/DropdownTestAsset.cs
+++ Assets/UGF.EditorTools.Editor.Tests/IMGUI.Dropdown/DropdownTestAsset.cs
@@ -1,9 +1,24 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace UGF.EditorTools.Editor.Tests.IMGUI.Dropdown
 {
     [CreateAssetMenu(menuName = "Tests/DropdownTestAsset")]
     public class DropdownTestAsset : ScriptableObject
     {
+        [SerializeField] private List<DataItem> m_items = new List<DataItem>();
+
+        public List<DataItem> Items { get { return m_items; } }
+
+        [Serializable]
+        public class DataItem
+        {
+            [SerializeField, DropdownTest] private string m_value;
+            [SerializeField] private bool m_bool;
+
+            public string Value { get { return m_value; } set { m_value = value; } }
+            public bool Bool { get { return m_bool; } set { m_bool = value; } }
+        }
     }
 }
---
+++ Assets/UGF.EditorTools.Editor.Tests/IMGUI.Dropdown/DropdownTestPropertyDrawer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UGF.EditorTools.Editor.IMGUI.Dropdown;
+using UnityEditor;
+using UnityEngine;
+
+namespace UGF.EditorTools.Editor.Tests.IMGUI.Dropdown
+{
+    public class DropdownTestAttribute : PropertyAttribute
+    {
+    }
+
+    [CustomPropertyDrawer(typeof(DropdownTestAttribute), true)]
+    public class DropdownTestPropertyDrawer : PropertyDrawer
+    {
+        private static readonly DropdownSelection<DropdownItem<string>> m_selection = new DropdownSelection<DropdownItem<string>>();
+
+        private static readonly List<DropdownItem<string>> m_items = new List<DropdownItem<string>>
+        {
+            new DropdownItem<string>("Item 0", "Value 0"),
+            new DropdownItem<string>("Item 1", "Value 1"),
+            new DropdownItem<string>("Item 2", "Value 2")
+        };
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            DropdownItem<string> item = m_items.Find(x => x.Value == property.stringValue) ?? new DropdownItem<string>("None", "");
+
+            DropdownItem<string> selected = DropdownEditorGUIUtility.Dropdown(position, label, new GUIContent(item.Name), m_selection, m_items, item);
+
+            if (item.Value != selected.Value)
+            {
+                property.stringValue = selected.Value;
+                property.serializedObject.ApplyModifiedProperties();
+            }
+        }
+    }
+}
