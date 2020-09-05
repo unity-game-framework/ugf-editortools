a09fa3aeee6f09d69a1339a90f1024fa20d6f012
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-09-05 21:20:33 +0300

    Update AssetGuidAttributeDrawer and AssetTypeAttributeDrawer

--- Packages/UGF.EditorTools/Editor/IMGUI/AssetTypeAttributeDrawer.cs
+++ Packages/UGF.EditorTools/Editor/IMGUI/AssetTypeAttributeDrawer.cs
@@ -1,30 +1,20 @@
-﻿using UGF.EditorTools.Runtime.IMGUI;
+﻿using UGF.EditorTools.Editor.IMGUI.PropertyDrawers;
+using UGF.EditorTools.Runtime.IMGUI;
 using UnityEditor;
 using UnityEngine;
 
 namespace UGF.EditorTools.Editor.IMGUI
 {
     [CustomPropertyDrawer(typeof(AssetTypeAttribute), true)]
-    internal class AssetTypeAttributeDrawer : PropertyDrawer
+    internal class AssetTypeAttributeDrawer : PropertyDrawerTyped<AssetTypeAttribute>
     {
-        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        public AssetTypeAttributeDrawer() : base(SerializedPropertyType.ObjectReference)
         {
-            EditorGUI.BeginProperty(position, label, property);
+        }
 
-            if (property.propertyType == SerializedPropertyType.ObjectReference)
-            {
-                var assetTypeAttribute = (AssetTypeAttribute)attribute;
-
-                EditorGUI.ObjectField(position, property, assetTypeAttribute.AssetType, label);
-            }
-            else
-            {
-                EditorGUI.PropertyField(position, property, label);
-
-                Debug.LogWarning($"Serialized property type must be 'ObjectReference' in order to use 'AssetTypeAttribute'. Property path: '{property.propertyPath}'.");
-            }
-
-            EditorGUI.EndProperty();
+        protected override void OnDrawProperty(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.ObjectField(position, property, Attribute.AssetType, label);
         }
     }
 }
