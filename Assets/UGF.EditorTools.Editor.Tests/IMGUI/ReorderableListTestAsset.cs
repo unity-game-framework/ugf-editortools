43f28c6fd73497b767fcab4de90172536123b83c
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-09-11 21:20:09 +0300

    Add ReorderableListDrawer

---
+++ Assets/UGF.EditorTools.Editor.Tests/IMGUI/ReorderableListTestAsset.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using UGF.EditorTools.Editor.IMGUI;
+using UGF.EditorTools.Runtime.IMGUI.References;
+using UnityEditor;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace UGF.EditorTools.Editor.Tests.IMGUI
+{
+    [CreateAssetMenu(menuName = "Tests/ReorderableListTestAsset")]
+    public class ReorderableListTestAsset : ScriptableObject
+    {
+        [SerializeField] private List<ScriptableObject> m_list1;
+        [SerializeField] private List<Data1> m_list2;
+        [SerializeField] private List<Data2> m_list3;
+        [SerializeReference, ManagedReference] private List<IData> m_list4;
+
+        public interface IData
+        {
+        }
+
+        [Serializable]
+        public class Data1 : IData
+        {
+            [SerializeField] private bool m_bool;
+            [SerializeField] private int m_int;
+            [SerializeField] private float m_float;
+        }
+
+        [Serializable]
+        public class Data2 : IData
+        {
+            [SerializeField] private bool m_bool2;
+            [SerializeField] private Object m_object;
+        }
+    }
+
+    [CustomEditor(typeof(ReorderableListTestAsset), true)]
+    public class ReorderableListTestAssetEditor : UnityEditor.Editor
+    {
+        private ReorderableListDrawer m_drawer1;
+        private ReorderableListDrawer m_drawer2;
+        private ReorderableListDrawer m_drawer3;
+        private ReorderableListDrawer m_drawer4;
+
+        private void OnEnable()
+        {
+            m_drawer1 = new ReorderableListDrawer(serializedObject.FindProperty("m_list1"));
+            m_drawer2 = new ReorderableListDrawer(serializedObject.FindProperty("m_list2"));
+            m_drawer3 = new ReorderableListDrawer(serializedObject.FindProperty("m_list3"));
+            m_drawer4 = new ReorderableListDrawer(serializedObject.FindProperty("m_list4"));
+        }
+
+        public override void OnInspectorGUI()
+        {
+            m_drawer1.DrawGUILayout();
+            m_drawer2.DrawGUILayout();
+            m_drawer3.DrawGUILayout();
+            m_drawer4.DrawGUILayout();
+        }
+    }
+}
---
+++ Packages/UGF.EditorTools/Editor/IMGUI/ReorderableListDrawer.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine;
+
+namespace UGF.EditorTools.Editor.IMGUI
+{
+    public class ReorderableListDrawer
+    {
+        public SerializedProperty SerializedProperty { get; }
+        public ReorderableList List { get; }
+
+        public ReorderableListDrawer(SerializedProperty serializedProperty)
+        {
+            SerializedProperty = serializedProperty ?? throw new ArgumentNullException(nameof(serializedProperty));
+            List = new ReorderableList(serializedProperty.serializedObject, serializedProperty)
+            {
+                drawHeaderCallback = OnDrawHeader,
+                drawElementCallback = OnDrawElement,
+                elementHeightCallback = OnElementHeight,
+                onAddCallback = OnAdd,
+                onRemoveCallback = OnRemove
+            };
+        }
+
+        public void DrawGUILayout()
+        {
+            List.DoLayoutList();
+        }
+
+        public void DrawGUI(Rect position)
+        {
+            List.DoList(position);
+        }
+
+        protected virtual void OnDrawHeader(Rect position)
+        {
+            GUI.Label(position, $"{SerializedProperty.displayName} (Size: {SerializedProperty.arraySize})", EditorStyles.boldLabel);
+        }
+
+        protected virtual void OnDrawElement(Rect position, int index, bool isActive, bool isFocused)
+        {
+            float height = EditorGUIUtility.singleLineHeight;
+            float space = EditorGUIUtility.standardVerticalSpacing;
+
+            position.height = height;
+            position.y += space;
+
+            SerializedProperty propertyElement = SerializedProperty.GetArrayElementAtIndex(index);
+
+            if (propertyElement.hasVisibleChildren)
+            {
+                position.xMin += height - space;
+            }
+
+            OnDrawElementContent(position, propertyElement, index, isActive, isFocused);
+        }
+
+        protected virtual void OnDrawElementContent(Rect position, SerializedProperty serializedProperty, int index, bool isActive, bool isFocused)
+        {
+            if (serializedProperty.propertyType == SerializedPropertyType.ObjectReference)
+            {
+                EditorGUI.PropertyField(position, serializedProperty, GUIContent.none);
+            }
+            else
+            {
+                EditorGUI.PropertyField(position, serializedProperty, true);
+            }
+        }
+
+        protected virtual float OnElementHeight(int index)
+        {
+            SerializedProperty propertyElement = SerializedProperty.GetArrayElementAtIndex(index);
+
+            float value = OnElementHeightContent(propertyElement, index);
+
+            value += EditorGUIUtility.standardVerticalSpacing * 2F;
+
+            return value;
+        }
+
+        protected virtual float OnElementHeightContent(SerializedProperty serializedProperty, int index)
+        {
+            return serializedProperty.propertyType == SerializedPropertyType.ObjectReference
+                ? EditorGUIUtility.singleLineHeight
+                : EditorGUI.GetPropertyHeight(serializedProperty, true);
+        }
+
+        protected virtual void OnAdd(ReorderableList list)
+        {
+            SerializedProperty.InsertArrayElementAtIndex(SerializedProperty.arraySize);
+
+            SerializedProperty propertyElement = SerializedProperty.GetArrayElementAtIndex(SerializedProperty.arraySize - 1);
+
+            switch (propertyElement.propertyType)
+            {
+                case SerializedPropertyType.ObjectReference:
+                {
+                    propertyElement.objectReferenceValue = null;
+                    break;
+                }
+                case SerializedPropertyType.ManagedReference:
+                {
+                    propertyElement.managedReferenceValue = null;
+                    break;
+                }
+            }
+
+            propertyElement.serializedObject.ApplyModifiedProperties();
+        }
+
+        protected virtual void OnRemove(ReorderableList list)
+        {
+            SerializedProperty.DeleteArrayElementAtIndex(list.index);
+            SerializedProperty.serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
