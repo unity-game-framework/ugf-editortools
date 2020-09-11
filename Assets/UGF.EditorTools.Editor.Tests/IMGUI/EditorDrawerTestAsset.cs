b0367c3a0c6c938ed2095d4a37b35f18fdecb950
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-09-11 21:58:13 +0300

    Add EditorDrawer

---
+++ Assets/UGF.EditorTools.Editor.Tests/IMGUI/EditorDrawerTestAsset.cs
@@ -0,0 +1,33 @@
+﻿using UGF.EditorTools.Editor.IMGUI;
+using UnityEditor;
+using UnityEngine;
+
+namespace UGF.EditorTools.Editor.Tests.IMGUI
+{
+    [CreateAssetMenu(menuName = "Tests/EditorDrawerTestAsset")]
+    public class EditorDrawerTestAsset : ScriptableObject
+    {
+        [SerializeField] private ScriptableObject m_target;
+    }
+
+    [CustomEditor(typeof(EditorDrawerTestAsset), true)]
+    public class EditorDrawerTestAssetEditor : UnityEditor.Editor
+    {
+        private SerializedProperty m_property;
+        private EditorDrawer m_drawer;
+
+        private void OnEnable()
+        {
+            m_property = serializedObject.FindProperty("m_target");
+            m_drawer = new EditorDrawer();
+        }
+
+        public override void OnInspectorGUI()
+        {
+            EditorGUILayout.PropertyField(m_property);
+
+            m_drawer.Set(m_property.objectReferenceValue);
+            m_drawer.DrawGUILayout();
+        }
+    }
+}
---
+++ Packages/UGF.EditorTools/Editor/IMGUI/EditorDrawer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Object = UnityEngine.Object;
+
+namespace UGF.EditorTools.Editor.IMGUI
+{
+    public class EditorDrawer
+    {
+        public UnityEditor.Editor Editor { get { return m_editor != null ? m_editor : throw new ArgumentException("Has no editor."); } }
+        public bool HasEditor { get { return m_editor != null; } }
+
+        private UnityEditor.Editor m_editor;
+
+        public void DrawGUILayout()
+        {
+            if (m_editor != null)
+            {
+                m_editor.OnInspectorGUI();
+            }
+        }
+
+        public bool Set(Object target)
+        {
+            if (m_editor == null || m_editor.target != target)
+            {
+                Clear();
+
+                if (target != null)
+                {
+                    m_editor = UnityEditor.Editor.CreateEditor(target);
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Clear()
+        {
+            if (m_editor != null)
+            {
+                Object.DestroyImmediate(m_editor);
+
+                m_editor = null;
+            }
+        }
+    }
+}
