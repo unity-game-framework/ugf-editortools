b104c335b43bb1aa33048fc1e22eff0c9832793b
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-09-18 01:06:04 +0300

    Add misc editor GUI scopes

---
+++ Packages/UGF.EditorTools/Editor/IMGUI.Scopes/GUIBackgroundColorScope.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace UGF.EditorTools.Editor.IMGUI.Scopes
+{
+    public readonly struct GUIBackgroundColorScope : IDisposable
+    {
+        private readonly Color m_color;
+
+        public GUIBackgroundColorScope(Color color)
+        {
+            m_color = GUI.backgroundColor;
+
+            GUI.backgroundColor = color;
+        }
+
+        public void Dispose()
+        {
+            GUI.backgroundColor = m_color;
+        }
+    }
+}
