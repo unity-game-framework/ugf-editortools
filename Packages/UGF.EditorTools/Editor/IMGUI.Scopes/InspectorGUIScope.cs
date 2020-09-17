85f066ca1f2cadd2d77d6cd8b58920327ab8fb09
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-09-18 01:11:45 +0300

    Fix label width settings inside of scope

--- Packages/UGF.EditorTools/Editor/IMGUI.Scopes/InspectorGUIScope.cs
+++ Packages/UGF.EditorTools/Editor/IMGUI.Scopes/InspectorGUIScope.cs
@@ -8,16 +8,19 @@ namespace UGF.EditorTools.Editor.IMGUI.Scopes
         private readonly bool m_hierarchyMode;
         private readonly bool m_wideMode;
         private readonly int m_indentLevel;
+        private readonly float m_labelWidth;
 
         public InspectorGUIScope(bool enable)
         {
             m_hierarchyMode = EditorGUIUtility.hierarchyMode;
             m_wideMode = EditorGUIUtility.wideMode;
             m_indentLevel = EditorGUI.indentLevel;
+            m_labelWidth = EditorGUIUtility.labelWidth;
 
             EditorGUIUtility.hierarchyMode = enable;
             EditorGUIUtility.wideMode = enable;
             EditorGUI.indentLevel++;
+            EditorGUIUtility.labelWidth = 0F;
         }
 
         public void Dispose()
@@ -25,6 +28,7 @@ namespace UGF.EditorTools.Editor.IMGUI.Scopes
             EditorGUIUtility.hierarchyMode = m_hierarchyMode;
             EditorGUIUtility.wideMode = m_wideMode;
             EditorGUI.indentLevel = m_indentLevel;
+            EditorGUIUtility.labelWidth = m_labelWidth;
         }
     }
 }
