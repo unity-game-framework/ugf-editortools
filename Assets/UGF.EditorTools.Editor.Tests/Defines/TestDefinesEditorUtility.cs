31affde1c6f837f870fb16cd69f763860b909a8a
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-09-22 23:22:38 +0300

    Add DefinesEditorUtility

---
+++ Assets/UGF.EditorTools.Editor.Tests/Defines/TestDefinesEditorUtility.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using UGF.EditorTools.Editor.Defines;
+using UnityEditor;
+
+namespace UGF.EditorTools.Editor.Tests.Defines
+{
+    public class TestDefinesEditorUtility
+    {
+        [Test]
+        public void SetDefine()
+        {
+            DefinesEditorUtility.SetDefine("TEST", BuildTargetGroup.Standalone);
+        }
+    }
+}
