29c3d32a519dbba7bc1ddab669c2d26ebd5dfa8b
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-09-23 22:35:17 +0300

    Update TestDefinesEditorUtility.cs

--- Assets/UGF.EditorTools.Editor.Tests/Defines/TestDefinesEditorUtility.cs
+++ Assets/UGF.EditorTools.Editor.Tests/Defines/TestDefinesEditorUtility.cs
@@ -6,10 +6,38 @@ namespace UGF.EditorTools.Editor.Tests.Defines
 {
     public class TestDefinesEditorUtility
     {
+        [TearDown]
+        public void CleanUp()
+        {
+            DefinesEditorUtility.RemoveDefine("TEST", BuildTargetGroup.Android);
+        }
+
+        [Test]
+        public void HasDefine()
+        {
+            DefinesEditorUtility.SetDefine("TEST", BuildTargetGroup.Android);
+
+            bool result = DefinesEditorUtility.HasDefine("TEST", BuildTargetGroup.Android);
+
+            Assert.True(result);
+        }
+
+        [Test]
+        public void RemoveDefine()
+        {
+            DefinesEditorUtility.SetDefine("TEST", BuildTargetGroup.Android);
+
+            bool result = DefinesEditorUtility.RemoveDefine("TEST", BuildTargetGroup.Android);
+
+            Assert.True(result);
+        }
+
         [Test]
         public void SetDefine()
         {
-            DefinesEditorUtility.SetDefine("TEST", BuildTargetGroup.Standalone);
+            bool result = DefinesEditorUtility.SetDefine("TEST", BuildTargetGroup.Standalone);
+
+            Assert.True(result);
         }
     }
 }
