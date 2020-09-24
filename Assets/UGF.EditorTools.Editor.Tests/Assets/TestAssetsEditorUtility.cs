f28aeee29a42e119fd1b883b04920c60edca5fd9
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-09-24 22:32:10 +0300

    Add AssetsEditorUtility

---
+++ Assets/UGF.EditorTools.Editor.Tests/Assets/TestAssetsEditorUtility.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using UGF.EditorTools.Editor.Assets;
+
+namespace UGF.EditorTools.Editor.Tests.Assets
+{
+    public class TestAssetsEditorUtility
+    {
+        [Test]
+        public void TryGetResourcesRelativePath()
+        {
+            string path1 = "Assets/Resources/Test/Folder/test-test.asset";
+            string path12 = "Assets/Resources/Test/Folder/resources.asset";
+            string path13 = "Assets/Resources/Test/Resources/Folder/resources.asset";
+            string path2 = "Resources/Folder";
+            string path3 = "Assets/Resources/";
+
+            bool result1 = AssetsEditorUtility.TryGetResourcesRelativePath(path1, out string pathResult1);
+            bool result12 = AssetsEditorUtility.TryGetResourcesRelativePath(path12, out string pathResult12);
+            bool result13 = AssetsEditorUtility.TryGetResourcesRelativePath(path13, out string pathResult13);
+            bool result2 = AssetsEditorUtility.TryGetResourcesRelativePath(path2, out string pathResult2);
+            bool result3 = AssetsEditorUtility.TryGetResourcesRelativePath(path3, out string pathResult3);
+
+            Assert.True(result1, $"pathResult1: {pathResult1}");
+            Assert.True(result12, $"pathResult12: {pathResult12}");
+            Assert.True(result13, $"pathResult13: {pathResult13}");
+            Assert.True(result2, $"pathResult2: {pathResult2}");
+            Assert.False(result3, $"pathResult3: {pathResult3}");
+            Assert.AreEqual("Test/Folder/test-test", pathResult1);
+            Assert.AreEqual("Test/Folder/resources", pathResult12);
+            Assert.AreEqual("Folder/resources", pathResult13);
+            Assert.AreEqual("Folder", pathResult2);
+            Assert.AreEqual(null, pathResult3);
+        }
+    }
+}
---
+++ Packages/UGF.EditorTools/Editor/Assets/AssetsEditorUtility.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace UGF.EditorTools.Editor.Assets
+{
+    public static class AssetsEditorUtility
+    {
+        public static string GetResourcesRelativePath(string path)
+        {
+            return TryGetResourcesRelativePath(path, out string result) ? result : throw new ArgumentException($"Can not find resources relative path from specified path: '{path}'.");
+        }
+
+        public static bool TryGetResourcesRelativePath(string path, out string result)
+        {
+            if (string.IsNullOrEmpty(path)) throw new ArgumentException("Value cannot be null or empty.", nameof(path));
+
+            var builder = new StringBuilder();
+            string directory = Path.GetDirectoryName(path);
+            string name = Path.GetFileNameWithoutExtension(path);
+
+            if (!string.IsNullOrEmpty(directory))
+            {
+                string resources = "resources";
+                int index = directory.LastIndexOf(resources, StringComparison.OrdinalIgnoreCase);
+
+                index += resources.Length + 1;
+
+                if (index < directory.Length)
+                {
+                    int length = directory.Length - index;
+
+                    if (length > 0)
+                    {
+                        directory = directory.Substring(index, length);
+                        directory = directory.Replace('\\', '/');
+
+                        builder.Append(directory);
+
+                        if (!string.IsNullOrEmpty(name))
+                        {
+                            builder.Append('/');
+                        }
+                    }
+                }
+            }
+
+            if (!string.IsNullOrEmpty(name))
+            {
+                builder.Append(name);
+            }
+
+            if (builder.Length > 0)
+            {
+                result = builder.ToString();
+                return true;
+            }
+
+            result = null;
+            return false;
+        }
+    }
+}
