e75c96241ad82af6fafffa2da5ce2f1d2fe121ad
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-09-25 20:07:32 +0300

    Add PopupMenuContent

---
+++ Packages/UGF.EditorTools/Editor/IMGUI.PopupMenus/PopupMenuContent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace UGF.EditorTools.Editor.IMGUI.PopupMenus
+{
+    public class PopupMenuContent : PopupWindowContent
+    {
+        public List<PopupMenuItem> Items { get; } = new List<PopupMenuItem>();
+
+        public override void OnGUI(Rect rect)
+        {
+        }
+    }
+}
---
+++ Packages/UGF.EditorTools/Editor/IMGUI.PopupMenus/PopupMenuItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace UGF.EditorTools.Editor.IMGUI.PopupMenus
+{
+    public class PopupMenuItem
+    {
+        public GUIContent Content { get; }
+        public bool Enabled { get; set; } = true;
+        public bool Priority { get; set; }
+
+        public PopupMenuItem(GUIContent content)
+        {
+            Content = content ?? throw new ArgumentNullException(nameof(content));
+        }
+    }
+}
