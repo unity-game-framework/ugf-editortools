d27c943f95609307ea6fab600c925375f3de990a
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-09-28 22:48:54 +0300

    Change PlatformEditorUtility

--- Packages/UGF.EditorTools/Editor/Platforms/PlatformEditorUtility.cs
+++ Packages/UGF.EditorTools/Editor/Platforms/PlatformEditorUtility.cs
@@ -1,6 +1,63 @@
-﻿namespace UGF.EditorTools.Editor.Platforms
+﻿using System;
+using UGF.EditorTools.Runtime.Platforms;
+using UnityEditor;
+
+namespace UGF.EditorTools.Editor.Platforms
 {
     public static class PlatformEditorUtility
     {
+        public static bool TrySetSettings<T>(SerializedProperty propertyGroups, T target, IPlatformSettings settings, PlatformGroupTargetCompareHandler<T> compareHandler)
+        {
+            if (settings == null) throw new ArgumentNullException(nameof(settings));
+
+            if (TryGetSettings(propertyGroups, target, out SerializedProperty propertySettings, compareHandler))
+            {
+                propertySettings.managedReferenceValue = settings;
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool TryGetSettings<T>(SerializedProperty propertyGroups, T target, out SerializedProperty propertySettings, PlatformGroupTargetCompareHandler<T> compareHandler)
+        {
+            if (TryGetGroup(propertyGroups, target, out SerializedProperty propertyGroup, compareHandler))
+            {
+                propertySettings = propertyGroup.FindPropertyRelative("m_settings");
+                return true;
+            }
+
+            propertySettings = null;
+            return false;
+        }
+
+        public static bool TryClearSettings<T>(SerializedProperty propertyGroups, T target, PlatformGroupTargetCompareHandler<T> compareHandler)
+        {
+            if (TryGetSettings(propertyGroups, target, out SerializedProperty propertySettings, compareHandler))
+            {
+                propertySettings.managedReferenceValue = null;
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool TryGetGroup<T>(SerializedProperty propertyGroups, T target, out SerializedProperty propertyGroup, PlatformGroupTargetCompareHandler<T> compareHandler)
+        {
+            if (compareHandler == null) throw new ArgumentNullException(nameof(compareHandler));
+
+            for (int i = 0; i < propertyGroups.arraySize; i++)
+            {
+                propertyGroup = propertyGroups.GetArrayElementAtIndex(i);
+
+                if (compareHandler(propertyGroup, target))
+                {
+                    return true;
+                }
+            }
+
+            propertyGroup = null;
+            return false;
+        }
     }
 }
---
+++ Packages/UGF.EditorTools/Editor/Platforms/PlatformGroupTargetCompareHandler.cs
@@ -0,0 +1,6 @@
+﻿using UnityEditor;
+
+namespace UGF.EditorTools.Editor.Platforms
+{
+    public delegate bool PlatformGroupTargetCompareHandler<in TTarget>(SerializedProperty serializedProperty, TTarget target);
+}
