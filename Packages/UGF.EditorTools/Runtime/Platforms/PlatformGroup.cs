0111df6426cfa6f3a46ddc0fc56227fe43a2a9ff
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-09-28 22:24:20 +0300

    Change PlatformSettings

--- Packages/UGF.EditorTools/Runtime/Platforms/PlatformGroup.cs
+++ Packages/UGF.EditorTools/Runtime/Platforms/PlatformGroup.cs
@@ -5,14 +5,30 @@ using UnityEngine;
 namespace UGF.EditorTools.Runtime.Platforms
 {
     [Serializable]
-    public class PlatformGroup<TTarget> : IPlatformGroup
+    public class PlatformGroup<TTarget>
     {
         [SerializeField] private TTarget m_target;
         [SerializeReference, ManagedReference] private IPlatformSettings m_settings;
 
         public TTarget Target { get { return m_target; } set { m_target = value; } }
-        public IPlatformSettings Settings { get { return m_settings; } set { m_settings = value; } }
+        public IPlatformSettings Settings { get { return m_settings ?? throw new ArgumentException("No settings specified."); } set { m_settings = value; } }
+        public bool HasSettings { get { return m_settings != null; } }
 
-        object IPlatformGroup.Target { get { return m_target; } }
+        public T GetSettings<T>() where T : IPlatformSettings
+        {
+            return TryGetSettings(out T settings) ? settings : throw new ArgumentException($"No settings with specified type: '{typeof(T)}'.");
+        }
+
+        public bool TryGetSettings<T>(out T settings) where T : IPlatformSettings
+        {
+            if (m_settings is T value)
+            {
+                settings = value;
+                return true;
+            }
+
+            settings = default;
+            return false;
+        }
     }
 }
