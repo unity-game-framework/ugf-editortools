4ebf3be7c83a2e30f31fe21dcd0d38e001a331ca
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-10-21 19:48:51 +0300

    Add AssetReference editors

---
+++ Packages/UGF.EditorTools/Editor/IMGUI.AssetReferences/AssetReferenceListDrawer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace UGF.EditorTools.Editor.IMGUI.AssetReferences
+{
+    public class AssetReferenceListDrawer : ReorderableListDrawer
+    {
+        public AssetReferenceListDrawer(SerializedProperty serializedProperty) : base(serializedProperty)
+        {
+        }
+
+        protected override void OnDrawElementContent(Rect position, SerializedProperty serializedProperty, int index, bool isActive, bool isFocused)
+        {
+            AssetReferenceEditorGUIUtility.AssetReference(position, GUIContent.none, serializedProperty);
+        }
+
+        protected override float OnElementHeightContent(SerializedProperty serializedProperty, int index)
+        {
+            return EditorGUIUtility.singleLineHeight;
+        }
+    }
+}
