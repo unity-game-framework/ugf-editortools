0dbd6a2b17054bc6ffead0398b3d49b3676bc540
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-11-18 21:54:28 +0300

    Mark TypesDropdownEditorUtility deprecated methods

--- Packages/UGF.EditorTools/Editor/IMGUI.Types/TypesDropdownEditorUtility.Deprecated.cs
+++ Packages/UGF.EditorTools/Editor/IMGUI.Types/TypesDropdownEditorUtility.Deprecated.cs
@@ -6,6 +6,7 @@ namespace UGF.EditorTools.Editor.IMGUI.Types
 {
     public static partial class TypesDropdownEditorUtility
     {
+        [Obsolete("GetTypeItems() method has been deprecated. Use GetTypeItems() method overload with collection argument instead.")]
         public static List<DropdownItem<Type>> GetTypeItems(Type targetType, bool useFullPath = false)
         {
             if (targetType == null) throw new ArgumentNullException(nameof(targetType));
@@ -17,6 +18,7 @@ namespace UGF.EditorTools.Editor.IMGUI.Types
             return items;
         }
 
+        [Obsolete("CreateItem() method has been deprecated. Use CreateItem() method overload with assembly name display argument.")]
         public static DropdownItem<Type> CreateItem(Type type, bool useFullPath = false)
         {
             return CreateItem(type, useFullPath, false);
