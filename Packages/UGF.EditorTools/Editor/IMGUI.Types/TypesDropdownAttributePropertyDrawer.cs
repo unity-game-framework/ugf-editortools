de2458e632be66096f77b286781100799ce4b852
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-11-18 23:05:56 +0300

    Add TypesDropdownAttributePropertyDrawer

---
+++ Packages/UGF.EditorTools/Editor/IMGUI.Types/TypesDropdownAttributePropertyDrawer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using UGF.EditorTools.Editor.IMGUI.Dropdown;
+using UGF.EditorTools.Runtime.IMGUI.Types;
+using UnityEditor;
+
+namespace UGF.EditorTools.Editor.IMGUI.Types
+{
+    [CustomPropertyDrawer(typeof(TypesDropdownAttribute), true)]
+    internal class TypesDropdownAttributePropertyDrawer : TypesDropdownAttributePropertyDrawer<TypesDropdownAttribute>
+    {
+        public TypesDropdownAttributePropertyDrawer() : base(SerializedPropertyType.String)
+        {
+        }
+
+        protected override DropdownDrawer<DropdownItem<Type>> OnCreateDrawer()
+        {
+            return new TypesDropdownDrawer(GetItems);
+        }
+
+        protected override void OnGetItems(ICollection<DropdownItem<Type>> items)
+        {
+            base.OnGetItems(items);
+
+            TypesDropdownEditorUtility.GetTypeItems(items, Attribute.TargetType, Attribute.DisplayFullPath, Attribute.DisplayAssemblyName);
+        }
+    }
+}
