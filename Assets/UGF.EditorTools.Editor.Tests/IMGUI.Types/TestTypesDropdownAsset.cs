2d69d6d18bbf56695616992bab4227490aa7b924
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-11-18 21:26:41 +0300

    Update tests

---
+++ Assets/UGF.EditorTools.Editor.Tests/IMGUI.Types/TestTypesDropdownAsset.cs
@@ -0,0 +1,14 @@
+﻿using UGF.EditorTools.Runtime.IMGUI.Types;
+using UnityEngine;
+
+namespace UGF.EditorTools.Editor.Tests.IMGUI.Types
+{
+    [CreateAssetMenu(menuName = "Tests/TestTypesDropdownAsset")]
+    public class TestTypesDropdownAsset : ScriptableObject
+    {
+        [TypesDropdown]
+        [SerializeField] private string m_type;
+
+        public string Type { get { return m_type; } set { m_type = value; } }
+    }
+}
