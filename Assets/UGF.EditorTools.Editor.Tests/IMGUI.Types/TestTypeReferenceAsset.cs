fea7042b23c4b43381d8037662043609d98b1ab8
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-11-19 19:14:39 +0300

    Add tests

---
+++ Assets/UGF.EditorTools.Editor.Tests/IMGUI.Types/TestTypeReferenceAsset.cs
@@ -0,0 +1,13 @@
+﻿using UGF.EditorTools.Runtime.IMGUI.Types;
+using UnityEngine;
+
+namespace UGF.EditorTools.Editor.Tests.IMGUI.Types
+{
+    [CreateAssetMenu(menuName = "Tests/TestTypeReferenceAsset")]
+    public class TestTypeReferenceAsset : ScriptableObject
+    {
+        [SerializeField] private TypeReference m_type;
+
+        public TypeReference Type { get { return m_type; } set { m_type = value; } }
+    }
+}
