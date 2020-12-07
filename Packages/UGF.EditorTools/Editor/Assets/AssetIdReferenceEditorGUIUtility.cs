8edc8231a4365b9d370a398f3987af11db8faf37
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-12-07 21:21:41 +0300

    Add AssetIdReference editor drawers

---
+++ Packages/UGF.EditorTools/Editor/Assets/AssetIdReferenceEditorGUIUtility.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UGF.EditorTools.Editor.Ids;
+using UnityEditor;
+using UnityEngine;
+
+namespace UGF.EditorTools.Editor.Assets
+{
+    public static class AssetIdReferenceEditorGUIUtility
+    {
+        public static void AssetIdReferenceField(Rect position, GUIContent label, SerializedProperty serializedProperty)
+        {
+            if (label == null) throw new ArgumentNullException(nameof(label));
+            if (serializedProperty == null) throw new ArgumentNullException(nameof(serializedProperty));
+
+            SerializedProperty propertyGuid = serializedProperty.FindPropertyRelative("m_guid");
+            SerializedProperty propertyAsset = serializedProperty.FindPropertyRelative("m_asset");
+
+            EditorGUI.ObjectField(position, propertyAsset, label);
+
+            string path = AssetDatabase.GetAssetPath(propertyAsset.objectReferenceValue);
+            string guid = AssetDatabase.AssetPathToGUID(path);
+
+            GlobalIdEditorUtility.SetGuidToProperty(propertyGuid, guid);
+        }
+    }
+}
