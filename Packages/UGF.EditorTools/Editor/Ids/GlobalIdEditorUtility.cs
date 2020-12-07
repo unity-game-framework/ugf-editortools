36194d4991c30ef53a544398dc59c5253c0f3949
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-12-07 20:56:08 +0300

    Add global id drawer

---
+++ Packages/UGF.EditorTools/Editor/Ids/GlobalIdEditorUtility.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using UGF.EditorTools.Runtime.Ids;
+using UnityEditor;
+
+namespace UGF.EditorTools.Editor.Ids
+{
+    public static class GlobalIdEditorUtility
+    {
+        public static string GetGuidFromProperty(SerializedProperty serializedProperty)
+        {
+            GlobalId id = GetGlobalIdFromProperty(serializedProperty);
+            string guid = id.ToString();
+
+            return guid;
+        }
+
+        public static bool SetGuidToProperty(SerializedProperty serializedProperty, string guid)
+        {
+            if (string.IsNullOrEmpty(guid)) throw new ArgumentException("Value cannot be null or empty.", nameof(guid));
+
+            if (GlobalId.TryParse(guid, out GlobalId id))
+            {
+                SetGlobalIdToProperty(serializedProperty, id);
+                return true;
+            }
+
+            return false;
+        }
+
+        public static GlobalId GetGlobalIdFromProperty(SerializedProperty serializedProperty)
+        {
+            if (serializedProperty == null) throw new ArgumentNullException(nameof(serializedProperty));
+
+            SerializedProperty propertyFirst = serializedProperty.FindPropertyRelative("m_first") ?? throw new ArgumentException("Property not found by the specified name: 'm_first'.");
+            SerializedProperty propertySecond = serializedProperty.FindPropertyRelative("m_second") ?? throw new ArgumentException("Property not found by the specified name: 'm_second'.");
+
+            ulong first = (ulong)propertyFirst.longValue;
+            ulong second = (ulong)propertySecond.longValue;
+            var id = new GlobalId(first, second);
+
+            return id;
+        }
+
+        public static void SetGlobalIdToProperty(SerializedProperty serializedProperty, GlobalId id)
+        {
+            if (serializedProperty == null) throw new ArgumentNullException(nameof(serializedProperty));
+
+            SerializedProperty propertyFirst = serializedProperty.FindPropertyRelative("m_first") ?? throw new ArgumentException("Property not found by the specified name: 'm_first'.");
+            SerializedProperty propertySecond = serializedProperty.FindPropertyRelative("m_second") ?? throw new ArgumentException("Property not found by the specified name: 'm_second'.");
+
+            propertyFirst.longValue = (long)id.First;
+            propertySecond.longValue = (long)id.Second;
+        }
+    }
+}
---
+++ Packages/UGF.EditorTools/Editor/Ids/GlobalIdPropertyDrawer.cs
@@ -0,0 +1,32 @@
+﻿using UGF.EditorTools.Editor.IMGUI.PropertyDrawers;
+using UGF.EditorTools.Runtime.Ids;
+using UnityEditor;
+using UnityEngine;
+
+namespace UGF.EditorTools.Editor.Ids
+{
+    [CustomPropertyDrawer(typeof(GlobalId), true)]
+    internal class GlobalIdPropertyDrawer : PropertyDrawerBase
+    {
+        protected override void OnDrawProperty(Rect position, SerializedProperty serializedProperty, GUIContent label)
+        {
+            string guid = GlobalIdEditorUtility.GetGuidFromProperty(serializedProperty);
+
+            guid = EditorGUI.TextField(position, label, guid);
+
+            if (!string.IsNullOrEmpty(guid))
+            {
+                GlobalIdEditorUtility.SetGuidToProperty(serializedProperty, guid);
+            }
+            else
+            {
+                GlobalIdEditorUtility.SetGlobalIdToProperty(serializedProperty, GlobalId.Empty);
+            }
+        }
+
+        public override float GetPropertyHeight(SerializedProperty serializedProperty, GUIContent label)
+        {
+            return EditorGUIUtility.singleLineHeight;
+        }
+    }
+}
