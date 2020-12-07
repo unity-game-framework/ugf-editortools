50d84b841793dee4a591fef0472bd8cf89fa58f0
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-12-07 20:56:25 +0300

    Add AssetIdAttribute

---
+++ Packages/UGF.EditorTools/Editor/Assets/AssetIdAttributePropertyDrawer.cs
@@ -0,0 +1,39 @@
+﻿using UGF.EditorTools.Editor.Ids;
+using UGF.EditorTools.Editor.IMGUI.Attributes;
+using UGF.EditorTools.Editor.IMGUI.PropertyDrawers;
+using UGF.EditorTools.Runtime.Assets;
+using UGF.EditorTools.Runtime.Ids;
+using UnityEditor;
+using UnityEngine;
+
+namespace UGF.EditorTools.Editor.Assets
+{
+    [CustomPropertyDrawer(typeof(AssetIdAttribute))]
+    internal class AssetIdAttributePropertyDrawer : PropertyDrawerTyped<AssetIdAttribute>
+    {
+        public AssetIdAttributePropertyDrawer() : base(SerializedPropertyType.Generic)
+        {
+        }
+
+        protected override void OnDrawProperty(Rect position, SerializedProperty serializedProperty, GUIContent label)
+        {
+            string guid = GlobalIdEditorUtility.GetGuidFromProperty(serializedProperty);
+
+            guid = AttributeEditorGUIUtility.DrawAssetGuidField(position, guid, label, Attribute.AssetType);
+
+            if (!string.IsNullOrEmpty(guid))
+            {
+                GlobalIdEditorUtility.SetGuidToProperty(serializedProperty, guid);
+            }
+            else
+            {
+                GlobalIdEditorUtility.SetGlobalIdToProperty(serializedProperty, GlobalId.Empty);
+            }
+        }
+
+        public override float GetPropertyHeight(SerializedProperty serializedProperty, GUIContent label)
+        {
+            return EditorGUIUtility.singleLineHeight;
+        }
+    }
+}
