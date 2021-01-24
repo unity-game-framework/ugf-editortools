9cded3b93d31032652bb5c77f785d6a07bfbe81c
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-01-24 19:37:10 +0300

    Update PlatformEditorUtility.cs

--- Packages/UGF.EditorTools/Editor/Platforms/PlatformEditorUtility.cs
+++ Packages/UGF.EditorTools/Editor/Platforms/PlatformEditorUtility.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using UnityEditor;
 using UnityEngine;
@@ -32,6 +33,48 @@ namespace UGF.EditorTools.Editor.Platforms
 
             PlatformsAll = new ReadOnlyCollection<PlatformInfo>(all);
             PlatformsAllAvailable = new ReadOnlyCollection<PlatformInfo>(available);
+        }
+
+        public static PlatformInfo GetPlatform(BuildTargetGroup buildTargetGroup)
+        {
+            return TryGetPlatform(buildTargetGroup, out PlatformInfo platform) ? platform : throw new ArgumentException($"Platform not found by the specified build target group: '{buildTargetGroup}'.");
+        }
+
+        public static bool TryGetPlatform(BuildTargetGroup buildTargetGroup, out PlatformInfo platform)
+        {
+            for (int i = 0; i < PlatformsAll.Count; i++)
+            {
+                platform = PlatformsAll[i];
+
+                if (platform.BuildTargetGroup == buildTargetGroup)
+                {
+                    return true;
+                }
+            }
+
+            platform = default;
+            return false;
+        }
+
+        public static PlatformInfo GetPlatform(BuildTarget buildTarget)
+        {
+            return TryGetPlatform(buildTarget, out PlatformInfo platform) ? platform : throw new ArgumentException($"Platform not found by the specified build target: '{buildTarget}'.");
+        }
+
+        public static bool TryGetPlatform(BuildTarget buildTarget, out PlatformInfo platform)
+        {
+            for (int i = 0; i < PlatformsAll.Count; i++)
+            {
+                platform = PlatformsAll[i];
+
+                if (platform.BuildTarget == buildTarget)
+                {
+                    return true;
+                }
+            }
+
+            platform = default;
+            return false;
         }
 
         private static void InternalGetPlatforms(ICollection<PlatformInfo> platforms)
