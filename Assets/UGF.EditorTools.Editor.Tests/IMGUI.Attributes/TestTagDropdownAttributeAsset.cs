4706f8eff65cf9b76463d0e2177e3ac4d97b0e30
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-02-09 21:45:02 +0300

    Add tests

---
+++ Assets/UGF.EditorTools.Editor.Tests/IMGUI.Attributes/TestTagDropdownAttributeAsset.cs
@@ -0,0 +1,13 @@
+﻿using UGF.EditorTools.Runtime.IMGUI.Attributes;
+using UnityEngine;
+
+namespace UGF.EditorTools.Editor.Tests.IMGUI.Attributes
+{
+    [CreateAssetMenu(menuName = "Tests/TestTagDropdownAttributeAsset")]
+    public class TestTagDropdownAttributeAsset : ScriptableObject
+    {
+        [SerializeField, TagDropdown] private string m_tag;
+
+        public string Tag { get { return m_tag; } set { m_tag = value; } }
+    }
+}
