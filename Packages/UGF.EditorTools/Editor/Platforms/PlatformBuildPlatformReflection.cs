9264cc21edbc3905ec73e2d67c086f861066809b
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-07-03 18:26:02 +0300

    Update PlatformBuildPlatformReflection.cs

--- Packages/UGF.EditorTools/Editor/Platforms/PlatformBuildPlatformReflection.cs
+++ Packages/UGF.EditorTools/Editor/Platforms/PlatformBuildPlatformReflection.cs
@@ -6,7 +6,7 @@ namespace UGF.EditorTools.Editor.Platforms
     internal class PlatformBuildPlatformReflection
     {
         public FieldInfo Name { get; }
-        public FieldInfo TargetGroup { get; }
+        public PropertyInfo TargetGroup { get; }
         public FieldInfo Tooltip { get; }
         public FieldInfo DefaultTarget { get; }
         public PropertyInfo Title { get; }
@@ -17,7 +17,7 @@ namespace UGF.EditorTools.Editor.Platforms
             var type = Type.GetType("UnityEditor.Build.BuildPlatform, UnityEditor.CoreModule", true);
 
             Name = type.GetField("name") ?? throw new ArgumentException("Field not found by the specified name: 'name'.");
-            TargetGroup = type.GetField("targetGroup") ?? throw new ArgumentException("Field not found by the specified name: 'targetGroup'.");
+            TargetGroup = type.GetProperty("targetGroup") ?? throw new ArgumentException("Property not found by the specified name: 'targetGroup'.");
             Tooltip = type.GetField("tooltip") ?? throw new ArgumentException("Field not found by the specified name: 'tooltip'.");
             DefaultTarget = type.GetField("defaultTarget") ?? throw new ArgumentException("Field not found by the specified name: 'defaultTarget'.");
             Title = type.GetProperty("title") ?? throw new ArgumentException("Property not found by the specified name: 'title'.");
