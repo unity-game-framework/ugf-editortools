7a62c9c31204f897fd31b0621facf21e6e13c8a2
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-08-02 20:42:28 +0300

    Add PreferenceEditorValue

---
+++ Packages/UGF.EditorTools/Editor/Preferences/PreferenceEditorValue.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UGF.EditorTools.Editor.IMGUI;
+
+namespace UGF.EditorTools.Editor.Preferences
+{
+    public class PreferenceEditorValue<TValue> : DrawerBase
+    {
+        public string Key { get; }
+        public TValue DefaultValue { get; }
+        public TValue Value { get; set; }
+
+        public PreferenceEditorValue(string key, TValue defaultValue = default)
+        {
+            if (string.IsNullOrEmpty(key)) throw new ArgumentException("Value cannot be null or empty.", nameof(key));
+
+            Key = key;
+            DefaultValue = defaultValue ?? throw new ArgumentNullException(nameof(defaultValue));
+        }
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+
+            Revert();
+        }
+
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+
+            Apply();
+        }
+
+        public void Apply()
+        {
+            PreferencesEditorUtility.SetValue(Key, Value);
+        }
+
+        public void Revert()
+        {
+            Value = PreferencesEditorUtility.GetValue(Key, DefaultValue);
+        }
+    }
+}
---
+++ Packages/UGF.EditorTools/Editor/Preferences/PreferencesEditorUtility.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEditor;
+
+namespace UGF.EditorTools.Editor.Preferences
+{
+    public static class PreferencesEditorUtility
+    {
+        public static T GetValue<T>(string key, T defaultValue = default)
+        {
+            if (string.IsNullOrEmpty(key)) throw new ArgumentException("Value cannot be null or empty.", nameof(key));
+
+            string text = EditorPrefs.GetString(key);
+
+            if (!string.IsNullOrEmpty(text))
+            {
+                var data = new PreferenceEditorValueData<T>();
+
+                EditorJsonUtility.FromJsonOverwrite(text, data);
+
+                return data.Value;
+            }
+
+            return defaultValue;
+        }
+
+        public static void SetValue<T>(string key, T value)
+        {
+            if (string.IsNullOrEmpty(key)) throw new ArgumentException("Value cannot be null or empty.", nameof(key));
+            if (value == null) throw new ArgumentNullException(nameof(value));
+
+            var data = new PreferenceEditorValueData<T> { Value = value };
+            string text = EditorJsonUtility.ToJson(data);
+
+            EditorPrefs.SetString(key, text);
+        }
+    }
+}
