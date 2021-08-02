fe2c929f83fbcd16c6758f58ef8595cf49f575ca
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-08-02 20:42:28 +0300

    Add PreferenceEditorValue

---
+++ Packages/UGF.EditorTools/Editor/Preferences/PreferenceEditorValueData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace UGF.EditorTools.Editor.Preferences
+{
+    [Serializable]
+    public class PreferenceEditorValueData<TValue>
+    {
+        [SerializeField] private TValue m_value;
+
+        public TValue Value { get { return m_value; } set { m_value = value; } }
+    }
+}
