251ade0d1f8c428385d19d9c8d17b84fec209e5d
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-11-22 22:01:27 +0300

    Add PackageEditorUtility

---
+++ Packages/UGF.EditorTools/Editor/Packages/PackageEditorUtility.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using PackageInfo = UnityEditor.PackageManager.PackageInfo;
+
+namespace UGF.EditorTools.Editor.Packages
+{
+    public static class PackageEditorUtility
+    {
+        public static PackageInfo GetPackage(string name)
+        {
+            return TryGetPackage(name, out PackageInfo package) ? package : throw new ArgumentException($"Package Info not found by the specified name: '{name}'.");
+        }
+
+        public static bool TryGetPackage(string name, out PackageInfo package)
+        {
+            if (string.IsNullOrEmpty(name)) throw new ArgumentException("Value cannot be null or empty.", nameof(name));
+
+            PackageInfo[] packages = PackageInfo.GetAllRegisteredPackages();
+
+            for (int i = 0; i < packages.Length; i++)
+            {
+                package = packages[i];
+
+                if (package.name == name)
+                {
+                    return true;
+                }
+            }
+
+            package = default;
+            return false;
+        }
+    }
+}
