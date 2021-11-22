2718495e25d98803c23e84f63a06ab8e3a5e9627
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-11-22 23:37:08 +0300

    Add ToolComponentHandle

---
+++ Packages/UGF.EditorTools/Editor/Tools/ToolComponentHandle.cs
@@ -0,0 +1,48 @@
+﻿using UGF.EditorTools.Editor.IMGUI.Scopes;
+using UnityEditor;
+using UnityEngine;
+
+namespace UGF.EditorTools.Editor.Tools
+{
+    public abstract class ToolComponentHandle : ToolComponent
+    {
+        protected virtual Matrix4x4 OnGetMatrix()
+        {
+            return Component.transform.localToWorldMatrix;
+        }
+
+        protected virtual void OnHandleSetup()
+        {
+        }
+
+        protected virtual void OnHandleChanged()
+        {
+        }
+
+        protected virtual void OnHandleDraw()
+        {
+        }
+
+        protected override void OnToolGUI()
+        {
+            using (new SerializedObjectUpdateScope(SerializedObject))
+            {
+                Matrix4x4 matrix = OnGetMatrix();
+
+                using (new Handles.DrawingScope(matrix))
+                {
+                    OnHandleSetup();
+
+                    EditorGUI.BeginChangeCheck();
+
+                    OnHandleDraw();
+
+                    if (EditorGUI.EndChangeCheck())
+                    {
+                        OnHandleChanged();
+                    }
+                }
+            }
+        }
+    }
+}
