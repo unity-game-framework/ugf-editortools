22093c6553b1c421088afe661e909a32c6a4c030
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-11-22 23:37:12 +0300

    Update ToolComponentBoundsHandle.cs

--- Packages/UGF.EditorTools/Editor/Tools/ToolComponentBoundsHandle.cs
+++ Packages/UGF.EditorTools/Editor/Tools/ToolComponentBoundsHandle.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using UGF.EditorTools.Editor.IMGUI.Scopes;
-using UnityEditor;
 using UnityEditor.IMGUI.Controls;
 using UnityEngine;
 
 namespace UGF.EditorTools.Editor.Tools
 {
-    public abstract class ToolComponentBoundsHandle<THandle> : ToolComponent where THandle : PrimitiveBoundsHandle
+    public abstract class ToolComponentBoundsHandle<THandle> : ToolComponentHandle where THandle : PrimitiveBoundsHandle
     {
         public THandle Handle { get; }
         public override GUIContent toolbarIcon { get; } = ToolEditorUtility.EditShapeContent;
@@ -16,39 +14,11 @@ namespace UGF.EditorTools.Editor.Tools
             Handle = handle ?? throw new ArgumentNullException(nameof(handle));
         }
 
-        protected virtual Matrix4x4 OnGetMatrix()
+        protected override void OnHandleDraw()
         {
-            return Component.transform.localToWorldMatrix;
-        }
+            base.OnHandleDraw();
 
-        protected virtual void OnHandleSetup()
-        {
-        }
-
-        protected virtual void OnHandleChanged()
-        {
-        }
-
-        protected override void OnToolGUI()
-        {
-            using (new SerializedObjectUpdateScope(SerializedObject))
-            {
-                Matrix4x4 matrix = OnGetMatrix();
-
-                using (new Handles.DrawingScope(matrix))
-                {
-                    OnHandleSetup();
-
-                    EditorGUI.BeginChangeCheck();
-
-                    Handle.DrawHandle();
-
-                    if (EditorGUI.EndChangeCheck())
-                    {
-                        OnHandleChanged();
-                    }
-                }
-            }
+            Handle.DrawHandle();
         }
     }
 }
