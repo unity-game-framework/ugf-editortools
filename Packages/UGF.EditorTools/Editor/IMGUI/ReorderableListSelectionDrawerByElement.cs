67a46f304d7b20f736061a44334b15e90937291d
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-11-22 22:50:28 +0300

    Add ReorderableListSelectionDrawer

---
+++ Packages/UGF.EditorTools/Editor/IMGUI/ReorderableListSelectionDrawerByElement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+
+namespace UGF.EditorTools.Editor.IMGUI
+{
+    public class ReorderableListSelectionDrawerByElement : ReorderableListSelectionDrawer
+    {
+        public ReorderableListSelectionDrawerByElement(ReorderableListDrawer listDrawer) : base(listDrawer)
+        {
+        }
+
+        protected override SerializedProperty OnGetObjectReferenceProperty(SerializedProperty propertyElement)
+        {
+            return propertyElement;
+        }
+    }
+}
