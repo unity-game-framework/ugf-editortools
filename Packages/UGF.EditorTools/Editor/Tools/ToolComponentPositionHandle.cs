b6ff2c6119dedf64e4441b111a2d50b3ba25da56
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-11-22 23:41:23 +0300

    Add ToolComponentPositionHandle

---
+++ Packages/UGF.EditorTools/Editor/Tools/ToolComponentPositionHandle.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace UGF.EditorTools.Editor.Tools
+{
+    public class ToolComponentPositionHandle : ToolComponentHandle
+    {
+        public string PropertyPositionName { get; }
+        public override GUIContent toolbarIcon { get; } = ToolEditorUtility.EditPositionContent;
+
+        protected SerializedProperty PropertyPosition { get { return m_propertyPosition ?? throw new ArgumentException("Value not specified."); } }
+
+        private SerializedProperty m_propertyPosition;
+        private Vector3 m_position;
+
+        public ToolComponentPositionHandle(string propertyPositionName)
+        {
+            if (string.IsNullOrEmpty(propertyPositionName)) throw new ArgumentException("Value cannot be null or empty.", nameof(propertyPositionName));
+
+            PropertyPositionName = propertyPositionName;
+        }
+
+        public override void OnActivated()
+        {
+            base.OnActivated();
+
+            m_propertyPosition = SerializedObject.FindProperty(PropertyPositionName);
+        }
+
+        public override void OnWillBeDeactivated()
+        {
+            base.OnWillBeDeactivated();
+
+            m_propertyPosition = null;
+        }
+
+        protected override void OnHandleSetup()
+        {
+            base.OnHandleSetup();
+
+            m_position = PropertyPosition.vector3Value;
+        }
+
+        protected override void OnHandleChanged()
+        {
+            base.OnHandleChanged();
+
+            PropertyPosition.vector3Value = m_position;
+        }
+
+        protected override void OnHandleDraw()
+        {
+            base.OnHandleDraw();
+
+            Quaternion rotation = UnityEditor.Tools.pivotRotation == PivotRotation.Local ? Component.transform.rotation : Quaternion.identity;
+
+            m_position = Handles.PositionHandle(m_position, rotation);
+        }
+    }
+}
