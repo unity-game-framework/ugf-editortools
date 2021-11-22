61bfcec4976f51eabc43cd9feaa28477ccaf92a1
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-11-22 23:11:17 +0300

    Add ToolComponentBoundsHandle

---
+++ Packages/UGF.EditorTools/Editor/Tools/ToolComponentBoundsHandle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UGF.EditorTools.Editor.IMGUI.Scopes;
+using UnityEditor;
+using UnityEditor.IMGUI.Controls;
+using UnityEngine;
+
+namespace UGF.EditorTools.Editor.Tools
+{
+    public abstract class ToolComponentBoundsHandle<THandle> : ToolComponent where THandle : PrimitiveBoundsHandle
+    {
+        public THandle Handle { get; }
+        public override GUIContent toolbarIcon { get; } = ToolEditorUtility.EditShapeContent;
+
+        protected ToolComponentBoundsHandle(THandle handle)
+        {
+            Handle = handle ?? throw new ArgumentNullException(nameof(handle));
+        }
+
+        protected abstract Matrix4x4 OnGetMatrix();
+        protected abstract void OnHandleSetup();
+        protected abstract void OnHandleChanged();
+
+        protected override void OnToolGUI()
+        {
+            using (new SerializedObjectUpdateScope(SerializedObject))
+            {
+                Matrix4x4 matrix = OnGetMatrix();
+
+                using (new Handles.DrawingScope(matrix))
+                {
+                    OnHandleSetup();
+
+                    EditorGUI.BeginChangeCheck();
+
+                    Handle.DrawHandle();
+
+                    if (EditorGUI.EndChangeCheck())
+                    {
+                        OnHandleChanged();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Packages/UGF.EditorTools/Editor/Tools/ToolEditorUtility.cs
@@ -0,0 +1,11 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace UGF.EditorTools.Editor.Tools
+{
+    public static class ToolEditorUtility
+    {
+        public static GUIContent EditPositionContent { get { return new GUIContent(EditorGUIUtility.IconContent("MoveTool").image, "Edit position."); } }
+        public static GUIContent EditShapeContent { get { return new GUIContent(EditorGUIUtility.IconContent("EditCollider").image, "Edit shape."); } }
+    }
+}
