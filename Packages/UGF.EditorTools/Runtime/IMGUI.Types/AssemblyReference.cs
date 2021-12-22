039f4c283a199a925615e208341706ccfdb7f488
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-12-22 20:28:14 +0300

    Add AssemblyReference

---
+++ Packages/UGF.EditorTools/Runtime/IMGUI.Types/AssemblyReference.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Reflection;
+using UnityEngine;
+
+namespace UGF.EditorTools.Runtime.IMGUI.Types
+{
+    [Serializable]
+    public struct AssemblyReference : IEquatable<AssemblyReference>, IComparable<AssemblyReference>
+    {
+        [SerializeField] private string m_value;
+
+        public string Value { get { return HasValue ? m_value : throw new ArgumentException("Value not specified."); } set { m_value = value; } }
+        public bool HasValue { get { return !string.IsNullOrEmpty(m_value); } }
+
+        public AssemblyReference(string value)
+        {
+            if (string.IsNullOrEmpty(value)) throw new ArgumentException("Value cannot be null or empty.", nameof(value));
+
+            m_value = value;
+        }
+
+        public Assembly Get()
+        {
+            return AssemblyUtility.GetAssemblyByFullName(m_value);
+        }
+
+        public void Set(Assembly assembly)
+        {
+            if (assembly == null) throw new ArgumentNullException(nameof(assembly));
+
+            m_value = assembly.FullName;
+        }
+
+        public void Clear()
+        {
+            m_value = string.Empty;
+        }
+
+        public bool Equals(AssemblyReference other)
+        {
+            return m_value == other.m_value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is AssemblyReference other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return m_value != null ? m_value.GetHashCode() : 0;
+        }
+
+        public int CompareTo(AssemblyReference other)
+        {
+            return string.Compare(m_value, other.m_value, StringComparison.Ordinal);
+        }
+
+        public static bool operator ==(AssemblyReference first, AssemblyReference second)
+        {
+            return first.Equals(second);
+        }
+
+        public static bool operator !=(AssemblyReference first, AssemblyReference second)
+        {
+            return !first.Equals(second);
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(m_value)}: {m_value}";
+        }
+    }
+}
---
+++ Packages/UGF.EditorTools/Runtime/IMGUI.Types/AssemblyUtility.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection;
+
+namespace UGF.EditorTools.Runtime.IMGUI.Types
+{
+    public static class AssemblyUtility
+    {
+        public static Assembly GetAssemblyByFullName(string fullName)
+        {
+            return TryGetAssemblyByFullName(fullName, out Assembly assembly) ? assembly : throw new ArgumentException($"Assembly not found by the specified full name: '{fullName}'.");
+        }
+
+        public static bool TryGetAssemblyByFullName(string fullName, out Assembly assembly)
+        {
+            if (string.IsNullOrEmpty(fullName)) throw new ArgumentException("Value cannot be null or empty.", nameof(fullName));
+
+            Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
+
+            for (int i = 0; i < assemblies.Length; i++)
+            {
+                assembly = assemblies[i];
+
+                if (assembly.FullName == fullName)
+                {
+                    return true;
+                }
+            }
+
+            assembly = default;
+            return false;
+        }
+    }
+}
