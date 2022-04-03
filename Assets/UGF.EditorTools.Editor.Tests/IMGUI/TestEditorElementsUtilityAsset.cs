103dc28bd44aa7b3770fcd9c79d2f47673d872c7
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2022-04-04 02:11:11 +0300

    Add tests

--- Assets/UGF.EditorTools.Editor.Tests/IMGUI/TestEditorElementsUtilityAsset.cs
+++ Assets/UGF.EditorTools.Editor.Tests/IMGUI/TestEditorElementsUtilityAsset.cs
@@ -13,10 +13,12 @@ namespace UGF.EditorTools.Editor.Tests.IMGUI
         [SerializeField] private string m_value;
         [SerializeField] private string m_value2;
         [SerializeField] private string m_value3;
+        [SerializeField] private long m_time;
 
         public string Value { get { return m_value; } set { m_value = value; } }
         public string Value2 { get { return m_value2; } set { m_value2 = value; } }
         public string Value3 { get { return m_value3; } set { m_value3 = value; } }
+        public long Time { get { return m_time; } set { m_time = value; } }
     }
 
     [CustomEditor(typeof(TestEditorElementsUtilityAsset), true)]
@@ -25,12 +27,14 @@ namespace UGF.EditorTools.Editor.Tests.IMGUI
         private SerializedProperty m_propertyValue;
         private SerializedProperty m_propertyValue2;
         private SerializedProperty m_propertyValue3;
+        private SerializedProperty m_propertyTime;
 
         private void OnEnable()
         {
             m_propertyValue = serializedObject.FindProperty("m_value");
             m_propertyValue2 = serializedObject.FindProperty("m_value2");
             m_propertyValue3 = serializedObject.FindProperty("m_value3");
+            m_propertyTime = serializedObject.FindProperty("m_time");
         }
 
         public override void OnInspectorGUI()
@@ -44,6 +48,8 @@ namespace UGF.EditorTools.Editor.Tests.IMGUI
                 {
                     EditorElementsUtility.TextFieldWithDropdown(m_propertyValue3, OnGetItems2);
                 }
+
+                EditorElementsUtility.TimeTicksField(m_propertyTime);
             }
         }
 
