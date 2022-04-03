da63917a5dd5d346ba595a86723baabe499329e8
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2022-04-04 02:15:48 +0300

    Add TimeTicksAttribute

---
+++ Packages/UGF.EditorTools/Runtime/IMGUI.Attributes/TimeTicksAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+namespace UGF.EditorTools.Runtime.IMGUI.Attributes
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public class TimeTicksAttribute : PropertyAttribute
+    {
+    }
+}
