7af14380e56ca78cf0a951c8d2f1b01021ea9347
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2022-04-04 02:15:53 +0300

    Add TimeTicksAttributePropertyDrawer

---
+++ Packages/UGF.EditorTools/Editor/IMGUI.Attributes/TimeTicksAttributePropertyDrawer.cs
@@ -0,0 +1,20 @@
+﻿using UGF.EditorTools.Editor.IMGUI.PropertyDrawers;
+using UGF.EditorTools.Runtime.IMGUI.Attributes;
+using UnityEditor;
+using UnityEngine;
+
+namespace UGF.EditorTools.Editor.IMGUI.Attributes
+{
+    [CustomPropertyDrawer(typeof(TimeTicksAttribute), true)]
+    internal class TimeTicksAttributePropertyDrawer : PropertyDrawerTyped<TimeTicksAttribute>
+    {
+        public TimeTicksAttributePropertyDrawer() : base(SerializedPropertyType.Integer)
+        {
+        }
+
+        protected override void OnDrawProperty(Rect position, SerializedProperty serializedProperty, GUIContent label)
+        {
+            EditorElementsUtility.TimeTicksField(position, label, serializedProperty);
+        }
+    }
+}
