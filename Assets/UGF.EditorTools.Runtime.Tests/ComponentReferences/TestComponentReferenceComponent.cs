0d3bc7bfda43a91ce6229ef2756bdf44ac626c2e
Vorobey <vorobyovvorobyov@gmail.com>
2022-07-11 19:29:10 +0300

    Add tests

--- Assets/UGF.EditorTools.Runtime.Tests/ComponentReferences/TestComponentReferenceComponent.cs
+++ Assets/UGF.EditorTools.Runtime.Tests/ComponentReferences/TestComponentReferenceComponent.cs
@@ -9,12 +9,16 @@ namespace UGF.EditorTools.Runtime.Tests.ComponentReferences
     {
         [SerializeField] private ComponentReference<Transform> m_reference;
         [SerializeField] private ComponentReference<Component> m_reference1;
+        [SerializeField] private ComponentIdReference<Camera> m_reference2;
         [SerializeField] private List<ComponentReference<Transform>> m_list = new List<ComponentReference<Transform>>();
         [SerializeField] private List<ComponentReference<Camera>> m_list2 = new List<ComponentReference<Camera>>();
+        [SerializeField] private List<ComponentIdReference<Camera>> m_list3 = new List<ComponentIdReference<Camera>>();
 
         public ComponentReference<Transform> Reference { get { return m_reference; } set { m_reference = value; } }
         public ComponentReference<Component> Reference1 { get { return m_reference1; } set { m_reference1 = value; } }
+        public ComponentIdReference<Camera> Reference2 { get { return m_reference2; } set { m_reference2 = value; } }
         public List<ComponentReference<Transform>> List { get { return m_list; } }
         public List<ComponentReference<Camera>> List2 { get { return m_list2; } }
+        public List<ComponentIdReference<Camera>> List3 { get { return m_list3; } }
     }
 }
