3d0a678ac9801226e33c0f03d0a2b74e67724a0f
Vorobey <vorobyovvorobyov@gmail.com>
2022-07-26 16:45:30 +0300

    Add SerializedPropertyEditorUtility

---
+++ Packages/UGF.EditorTools/Editor/Serialized/SerializedPropertyEditorUtility.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+
+namespace UGF.EditorTools.Editor.Serialized
+{
+    public static class SerializedPropertyEditorUtility
+    {
+        public static IEnumerable<SerializedProperty> GetChildren(SerializedProperty serializedProperty)
+        {
+            if (serializedProperty == null) throw new ArgumentNullException(nameof(serializedProperty));
+
+            SerializedProperty propertyEnd = serializedProperty.GetEndProperty();
+
+            serializedProperty.NextVisible(true);
+
+            if (!SerializedProperty.EqualContents(serializedProperty, propertyEnd))
+            {
+                yield return serializedProperty;
+
+                while (serializedProperty.NextVisible(false) && !SerializedProperty.EqualContents(serializedProperty, propertyEnd))
+                {
+                    yield return serializedProperty;
+                }
+            }
+        }
+    }
+}
