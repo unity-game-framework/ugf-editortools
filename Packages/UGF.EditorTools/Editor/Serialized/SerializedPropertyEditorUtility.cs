05217f755f649f24c93bcfa604d5aba0c991f3f2
Vorobey <vorobyovvorobyov@gmail.com>
2022-07-29 20:45:13 +0300

    Update SerializedPropertyEditorUtility.cs

--- Packages/UGF.EditorTools/Editor/Serialized/SerializedPropertyEditorUtility.cs
+++ Packages/UGF.EditorTools/Editor/Serialized/SerializedPropertyEditorUtility.cs
@@ -1,11 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEditor;
 
 namespace UGF.EditorTools.Editor.Serialized
 {
     public static class SerializedPropertyEditorUtility
     {
+        private static readonly GetFieldInfoFromPropertyHandler m_getFieldInfoFromProperty;
+
+        private delegate FieldInfo GetFieldInfoFromPropertyHandler(SerializedProperty serializedProperty, out Type type);
+
+        static SerializedPropertyEditorUtility()
+        {
+            var type = Type.GetType("UnityEditor.ScriptAttributeUtility, UnityEditor.CoreModule", true);
+
+            MethodInfo getFieldInfoFromPropertyMethod = type.GetMethod("GetFieldInfoFromProperty", BindingFlags.NonPublic | BindingFlags.Static)
+                                                        ?? throw new ArgumentException("Method not found by the specified name: 'GetFieldInfoFromProperty'.");
+
+            m_getFieldInfoFromProperty = (GetFieldInfoFromPropertyHandler)getFieldInfoFromPropertyMethod.CreateDelegate(typeof(GetFieldInfoFromPropertyHandler));
+        }
+
+        public static Type GetFieldType(SerializedProperty serializedProperty)
+        {
+            FieldInfo fieldInfo = GetFieldInfoAndType(serializedProperty, out _);
+
+            return fieldInfo.FieldType;
+        }
+
+        public static FieldInfo GetFieldInfoAndType(SerializedProperty serializedProperty, out Type type)
+        {
+            if (serializedProperty == null) throw new ArgumentNullException(nameof(serializedProperty));
+
+            return m_getFieldInfoFromProperty.Invoke(serializedProperty, out type);
+        }
+
         public static IEnumerable<SerializedProperty> GetChildrenVisible(SerializedProperty serializedProperty)
         {
             if (serializedProperty == null) throw new ArgumentNullException(nameof(serializedProperty));
