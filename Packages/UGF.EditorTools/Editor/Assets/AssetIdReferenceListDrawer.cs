eb8c862eb974da0ed8213008a605dec681395e00
Vorobey <vorobyovvorobyov@gmail.com>
2022-07-29 20:45:20 +0300

    Update AssetIdReferenceListDrawer.cs

--- Packages/UGF.EditorTools/Editor/Assets/AssetIdReferenceListDrawer.cs
+++ Packages/UGF.EditorTools/Editor/Assets/AssetIdReferenceListDrawer.cs
@@ -1,28 +1,70 @@
-﻿using UGF.EditorTools.Editor.IMGUI;
+﻿using System;
+using UGF.EditorTools.Editor.Ids;
+using UGF.EditorTools.Editor.IMGUI;
+using UGF.EditorTools.Editor.IMGUI.Attributes;
+using UGF.EditorTools.Editor.Serialized;
+using UGF.EditorTools.Runtime.Ids;
 using UnityEditor;
 using UnityEngine;
 
 namespace UGF.EditorTools.Editor.Assets
 {
-    public class AssetIdReferenceListDrawer : ReorderableListDrawer
+    public class AssetIdReferenceListDrawer : ReorderableListKeyAndValueDrawer
     {
-        public AssetIdReferenceListDrawer(SerializedProperty serializedProperty) : base(serializedProperty)
+        public bool DisplayAsReplace { get; set; }
+
+        private Type m_assetType;
+
+        public AssetIdReferenceListDrawer(SerializedProperty serializedProperty) : base(serializedProperty, "m_guid", "m_asset")
         {
+            DisplayAsSingleLine = true;
+        }
+
+        public void DrawReplaceControlsLayout()
+        {
+            EditorGUILayout.Space();
+
+            using (new EditorGUILayout.HorizontalScope())
+            {
+                GUILayout.FlexibleSpace();
+
+                DrawReplaceButtonLayout();
+            }
+        }
+
+        public void DrawReplaceButtonLayout()
+        {
+            DisplayAsReplace = GUILayout.Toggle(DisplayAsReplace, "Replace", EditorStyles.miniButton);
         }
 
         protected override void OnDrawElementContent(Rect position, SerializedProperty serializedProperty, int index, bool isActive, bool isFocused)
         {
-            AssetIdReferenceEditorGUIUtility.AssetIdReferenceField(position, GUIContent.none, serializedProperty);
+            SerializedProperty propertyKey = serializedProperty.FindPropertyRelative(PropertyKeyName);
+            SerializedProperty propertyValue = serializedProperty.FindPropertyRelative(PropertyValueName);
+
+            m_assetType ??= SerializedPropertyEditorUtility.GetFieldType(propertyValue);
+
+            string guid = AssetDatabase.AssetPathToGUID(AssetDatabase.GetAssetPath(propertyValue.objectReferenceValue));
+            GlobalId idKey = GlobalIdEditorUtility.GetGlobalIdFromProperty(propertyKey);
+            GlobalId idAsset = !string.IsNullOrEmpty(guid) && GlobalId.TryParse(guid, out GlobalId result) ? result : GlobalId.Empty;
+
+            if ((idKey != GlobalId.Empty && idKey != idAsset) || DisplayAsReplace)
+            {
+                base.OnDrawElementContent(position, serializedProperty, index, isActive, isFocused);
+            }
+            else
+            {
+                AssetIdReferenceEditorGUIUtility.AssetIdReferenceField(position, GUIContent.none, serializedProperty);
+            }
         }
 
-        protected override float OnElementHeightContent(SerializedProperty serializedProperty, int index)
+        protected override void OnDrawKey(Rect position, SerializedProperty serializedProperty)
         {
-            return EditorGUIUtility.singleLineHeight;
-        }
+            string guid = GlobalIdEditorUtility.GetGuidFromProperty(serializedProperty);
 
-        protected override bool OnElementHasVisibleChildren(SerializedProperty serializedProperty)
-        {
-            return false;
+            guid = AttributeEditorGUIUtility.DrawAssetGuidField(position, guid, GUIContent.none, m_assetType);
+
+            GlobalIdEditorUtility.SetGuidToProperty(serializedProperty, guid);
         }
     }
 }
