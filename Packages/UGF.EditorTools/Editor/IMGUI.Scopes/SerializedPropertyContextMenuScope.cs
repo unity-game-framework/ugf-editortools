f191d200ad27492446fad680bc74f373d3c76c58
Vorobey <vorobyovvorobyov@gmail.com>
2022-07-30 16:27:42 +0300

    Add SerializedPropertyContextMenuScope

---
+++ Packages/UGF.EditorTools/Editor/IMGUI.Scopes/SerializedPropertyContextMenuScope.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEditor;
+
+namespace UGF.EditorTools.Editor.IMGUI.Scopes
+{
+    public readonly struct SerializedPropertyContextMenuScope : IDisposable
+    {
+        private readonly EditorApplication.SerializedPropertyCallbackFunction m_handler;
+
+        public SerializedPropertyContextMenuScope(EditorApplication.SerializedPropertyCallbackFunction handler)
+        {
+            m_handler = handler ?? throw new ArgumentNullException(nameof(handler));
+
+            EditorApplication.contextualPropertyMenu += m_handler;
+        }
+
+        public void Dispose()
+        {
+            EditorApplication.contextualPropertyMenu -= m_handler;
+        }
+    }
+}
