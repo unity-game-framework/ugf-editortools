27b1d1f59d4c763551e15ce672474d5e663eaa09
Vorobey <vorobyovvorobyov@gmail.com>
2022-07-30 16:27:38 +0300

    Update SerializedPropertyGUIScope.cs

--- Packages/UGF.EditorTools/Editor/IMGUI.Scopes/SerializedPropertyGUIScope.cs
+++ Packages/UGF.EditorTools/Editor/IMGUI.Scopes/SerializedPropertyGUIScope.cs
@@ -8,6 +8,9 @@ namespace UGF.EditorTools.Editor.IMGUI.Scopes
     {
         public SerializedPropertyGUIScope(Rect position, GUIContent label, SerializedProperty serializedProperty)
         {
+            if (label == null) throw new ArgumentNullException(nameof(label));
+            if (serializedProperty == null) throw new ArgumentNullException(nameof(serializedProperty));
+
             EditorGUI.BeginProperty(position, label, serializedProperty);
         }
 
