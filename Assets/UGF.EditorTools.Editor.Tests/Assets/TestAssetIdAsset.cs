5c0b9f657ded4bea8352c462c3895d2ff438017d
Vorobey <vorobyovvorobyov@gmail.com>
2022-07-30 16:14:56 +0300

    Change tests

--- Assets/UGF.EditorTools.Editor.Tests/Assets/TestAssetIdAsset.cs
+++ Assets/UGF.EditorTools.Editor.Tests/Assets/TestAssetIdAsset.cs
@@ -7,11 +7,15 @@ namespace UGF.EditorTools.Editor.Tests.Assets
     [CreateAssetMenu(menuName = "Tests/TestAssetIdAsset")]
     public class TestAssetIdAsset : ScriptableObject
     {
-        [SerializeField, AssetId] private GlobalId m_id;
+        [AssetId]
+        [SerializeField] private GlobalId m_id;
         [AssetId(typeof(Material))]
         [SerializeField] private GlobalId m_material;
+        [AssetId(typeof(Material))]
+        [SerializeField] private GlobalId m_material2;
 
         public GlobalId Id { get { return m_id; } set { m_id = value; } }
         public GlobalId Material { get { return m_material; } set { m_material = value; } }
+        public GlobalId Material2 { get { return m_material2; } set { m_material2 = value; } }
     }
 }
