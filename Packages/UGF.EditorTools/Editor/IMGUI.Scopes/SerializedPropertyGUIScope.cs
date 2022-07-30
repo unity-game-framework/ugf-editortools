727cb09259f29934549865eca890e69e602b3019
Vorobey <vorobyovvorobyov@gmail.com>
2022-07-30 16:25:57 +0300

    Add SerializedPropertyGUIScope

---
+++ Packages/UGF.EditorTools/Editor/IMGUI.Scopes/SerializedPropertyGUIScope.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace UGF.EditorTools.Editor.IMGUI.Scopes
+{
+    public readonly struct SerializedPropertyGUIScope : IDisposable
+    {
+        public SerializedPropertyGUIScope(Rect position, GUIContent label, SerializedProperty serializedProperty)
+        {
+            EditorGUI.BeginProperty(position, label, serializedProperty);
+        }
+
+        public void Dispose()
+        {
+            EditorGUI.EndProperty();
+        }
+    }
+}
