a61caeebccf01fc3df362965260efa07269eda76
Vorobey <vorobyovvorobyov@gmail.com>
2022-09-18 19:31:56 +0300

    Update TypeReferenceDropdownAttribute.cs

--- Packages/UGF.EditorTools/Runtime/IMGUI.Types/TypeReferenceDropdownAttribute.cs
+++ Packages/UGF.EditorTools/Runtime/IMGUI.Types/TypeReferenceDropdownAttribute.cs
@@ -6,8 +6,11 @@ namespace UGF.EditorTools.Runtime.IMGUI.Types
     {
         public override Type TargetType { get { return m_targetType ?? throw new ArgumentException("Target type not specified."); } }
         public bool HasTargetType { get { return m_targetType != null; } }
+        public Type AttributeType { get { return m_attributeType ?? throw new ArgumentException("Value not specified."); } set { m_attributeType = value ?? throw new ArgumentNullException(nameof(value)); } }
+        public bool HasAttributeType { get { return m_attributeType != null; } }
 
         private readonly Type m_targetType;
+        private Type m_attributeType;
 
         public TypeReferenceDropdownAttribute(Type targetType = null)
         {
