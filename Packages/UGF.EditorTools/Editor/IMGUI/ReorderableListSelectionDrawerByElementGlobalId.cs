033e6a1a8bd8328ed5d58314a5e928934acccc05
Vorobey <vorobyovvorobyov@gmail.com>
2022-12-03 17:08:23 +0100

    Add selection for global id

---
+++ Packages/UGF.EditorTools/Editor/IMGUI/ReorderableListSelectionDrawerByElementGlobalId.cs
@@ -0,0 +1,22 @@
+﻿using UGF.EditorTools.Editor.Ids;
+using UnityEditor;
+using UnityEngine;
+
+namespace UGF.EditorTools.Editor.IMGUI
+{
+    public class ReorderableListSelectionDrawerByElementGlobalId : ReorderableListSelectionDrawerByElement
+    {
+        public ReorderableListSelectionDrawerByElementGlobalId(ReorderableListDrawer listDrawer) : base(listDrawer)
+        {
+        }
+
+        protected override bool OnTryGetTarget(SerializedProperty serializedProperty, out Object target)
+        {
+            string guid = GlobalIdEditorUtility.GetGuidFromProperty(serializedProperty);
+
+            target = AssetDatabase.LoadAssetAtPath<Object>(AssetDatabase.GUIDToAssetPath(guid));
+
+            return target != null;
+        }
+    }
+}
---
+++ Packages/UGF.EditorTools/Editor/IMGUI/ReorderableListSelectionDrawerByPathGlobalId.cs
@@ -0,0 +1,22 @@
+﻿using UGF.EditorTools.Editor.Ids;
+using UnityEditor;
+using UnityEngine;
+
+namespace UGF.EditorTools.Editor.IMGUI
+{
+    public class ReorderableListSelectionDrawerByPathGlobalId : ReorderableListSelectionDrawerByPath
+    {
+        public ReorderableListSelectionDrawerByPathGlobalId(ReorderableListDrawer listDrawer, string propertyPath) : base(listDrawer, propertyPath)
+        {
+        }
+
+        protected override bool OnTryGetTarget(SerializedProperty serializedProperty, out Object target)
+        {
+            string guid = GlobalIdEditorUtility.GetGuidFromProperty(serializedProperty);
+
+            target = AssetDatabase.LoadAssetAtPath<Object>(AssetDatabase.GUIDToAssetPath(guid));
+
+            return target != null;
+        }
+    }
+}
