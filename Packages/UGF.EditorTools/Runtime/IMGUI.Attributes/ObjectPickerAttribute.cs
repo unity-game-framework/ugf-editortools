a6bf57f20b948c024eaa76cf3b5779ce26f766aa
Vorobey <vorobyovvorobyov@gmail.com>
2023-11-16 18:41:56 +0300

    Add ObjectPickerAttribute

---
+++ Packages/UGF.EditorTools/Runtime/IMGUI.Attributes/ObjectPickerAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace UGF.EditorTools.Runtime.IMGUI.Attributes
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public class ObjectPickerAttribute : PropertyAttribute
+    {
+        public Type TargetType { get; }
+        public string Filter { get; }
+
+        public ObjectPickerAttribute(string filter = "") : this(typeof(Object), filter)
+        {
+        }
+
+        public ObjectPickerAttribute(Type targetType, string filter = "")
+        {
+            TargetType = targetType ?? typeof(Object);
+            Filter = filter ?? throw new ArgumentNullException(nameof(filter));
+        }
+    }
+}
