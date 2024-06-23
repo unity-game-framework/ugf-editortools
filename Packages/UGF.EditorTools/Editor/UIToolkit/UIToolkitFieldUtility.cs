161656a070b9bf32f0584f4cde79c3db9c5b7a1f
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2024-06-23 21:47:35 +0300

    Add UIToolkitFieldUtility

---
+++ Packages/UGF.EditorTools/Editor/UIToolkit/UIToolkitFieldUtility.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine.UIElements;
+
+namespace UGF.EditorTools.Editor.UIToolkit
+{
+    public static class UIToolkitFieldUtility
+    {
+        public static void SetClasses<T>(BaseField<T> field)
+        {
+            if (field == null) throw new ArgumentNullException(nameof(field));
+
+            field.AddToClassList(BaseField<T>.alignedFieldUssClassName);
+        }
+    }
+}
