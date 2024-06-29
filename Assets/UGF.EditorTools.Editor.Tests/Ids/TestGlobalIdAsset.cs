49f3cf18d654c97c4ff5fcd0739ad7cab93d6e49
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2024-06-29 16:32:58 +0300

    Update TestGlobalIdAsset.cs

--- Assets/UGF.EditorTools.Editor.Tests/Ids/TestGlobalIdAsset.cs
+++ Assets/UGF.EditorTools.Editor.Tests/Ids/TestGlobalIdAsset.cs
@@ -8,10 +8,12 @@ namespace UGF.EditorTools.Editor.Tests.Ids
     public class TestGlobalIdAsset : ScriptableObject
     {
         [SerializeField] private GlobalId m_id;
+        [SerializeField] private GlobalId m_id3;
         [AssetId(typeof(Material))]
         [SerializeField] private GlobalId m_id2;
 
         public GlobalId Id { get { return m_id; } set { m_id = value; } }
+        public GlobalId ID3 { get { return m_id3; } set { m_id3 = value; } }
         public GlobalId ID2 { get { return m_id2; } set { m_id2 = value; } }
     }
 }
