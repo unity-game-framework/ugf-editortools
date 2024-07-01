a84ab25f2691a4c348620845dc629b5081b891a5
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2024-07-01 20:06:47 +0300

    Change UIToolkitEditorUtility

--- Packages/UGF.EditorTools/Editor/UIToolkit/UIToolkitEditorUtility.cs
+++ Packages/UGF.EditorTools/Editor/UIToolkit/UIToolkitEditorUtility.cs
@@ -1,10 +1,18 @@
 ﻿using System;
+using UnityEditor;
 using UnityEngine.UIElements;
 
 namespace UGF.EditorTools.Editor.UIToolkit
 {
     public static class UIToolkitEditorUtility
     {
+        public static StyleSheet StyleSheet { get; }
+
+        static UIToolkitEditorUtility()
+        {
+            StyleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Packages/com.ugf.editortools/Data.Editor/UGF.EditorTools.Editor.Styles.uss") ?? throw new ArgumentException("UIToolkit Styles not found.");
+        }
+
         public static void AddFieldClasses<T>(BaseField<T> field)
         {
             if (field == null) throw new ArgumentNullException(nameof(field));
