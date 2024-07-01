58791c30f9a89dae851624b23a93fb54fd16c6eb
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2024-07-01 23:35:44 +0300

    Add ObjectFieldLinkedElement

---
+++ Packages/com.ugf.editortools/Editor/UIToolkit.Elements/ObjectFieldLinkedElement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+
+namespace UGF.EditorTools.Editor.UIToolkit.Elements
+{
+    public class ObjectFieldLinkedElement : ObjectField
+    {
+        public Button LinkButtonElement { get; }
+        public static string LinkButtonUssClassName { get; } = "ugf-object-field-linked__button";
+
+        public ObjectFieldLinkedElement()
+        {
+            LinkButtonElement = new Button();
+            LinkButtonElement.AddToClassList(LinkButtonUssClassName);
+
+            VisualElement inputElement = this.Query<VisualElement>(className: inputUssClassName).First();
+            VisualElement selectorElement = inputElement.Query<VisualElement>(className: selectorUssClassName).First();
+
+            inputElement.Insert(inputElement.IndexOf(selectorElement), LinkButtonElement);
+            styleSheets.Add(UIToolkitEditorUtility.StyleSheet);
+        }
+    }
+}
--- Packages/com.ugf.editortools/Editor/UIToolkit/UIToolkitEditorUtility.cs
+++ Packages/com.ugf.editortools/Editor/UIToolkit/UIToolkitEditorUtility.cs
@@ -7,10 +7,14 @@ namespace UGF.EditorTools.Editor.UIToolkit
     public static class UIToolkitEditorUtility
     {
         public static StyleSheet StyleSheet { get; }
+        public static ThemeStyleSheet StyleSheetLight { get; }
+        public static ThemeStyleSheet StyleSheetDark { get; }
 
         static UIToolkitEditorUtility()
         {
             StyleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Packages/com.ugf.editortools/Data.Editor/UGF.EditorTools.Editor.Styles.uss") ?? throw new ArgumentException("UIToolkit Styles not found.");
+            StyleSheetLight = AssetDatabase.LoadAssetAtPath<ThemeStyleSheet>("Packages/com.ugf.editortools/Data.Editor/UGF.EditorTools.Editor.Styles.Light.tss") ?? throw new ArgumentException("UIToolkit Styles not found.");
+            StyleSheetDark = AssetDatabase.LoadAssetAtPath<ThemeStyleSheet>("Packages/com.ugf.editortools/Data.Editor/UGF.EditorTools.Editor.Styles.Dark.tss") ?? throw new ArgumentException("UIToolkit Styles not found.");
         }
 
         public static void AddFieldClasses<T>(BaseField<T> field)
