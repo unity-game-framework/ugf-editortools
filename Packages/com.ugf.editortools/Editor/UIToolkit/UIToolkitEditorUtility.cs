7e5cd5508d226a08ad4acb14ac405bedece95670
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2024-07-07 12:31:54 +0300

    Update UIToolkitEditorUtility.cs

--- Packages/com.ugf.editortools/Editor/UIToolkit/UIToolkitEditorUtility.cs
+++ Packages/com.ugf.editortools/Editor/UIToolkit/UIToolkitEditorUtility.cs
@@ -6,15 +6,28 @@ namespace UGF.EditorTools.Editor.UIToolkit
 {
     public static class UIToolkitEditorUtility
     {
+        public static string DataDirectoryPath { get; } = "Packages/com.ugf.editortools/Editor.Data";
+        public static string StyleSheetPath { get; } = $"{DataDirectoryPath}/UGF.EditorTools.Editor.Styles.uss";
+        public static string StyleSheetThemeLightPath { get; } = $"{DataDirectoryPath}/UGF.EditorTools.Editor.Styles.Light.tss";
+        public static string StyleSheetThemeDarkPath { get; } = $"{DataDirectoryPath}/UGF.EditorTools.Editor.Styles.Dark.tss";
         public static StyleSheet StyleSheet { get; }
-        public static ThemeStyleSheet StyleSheetLight { get; }
-        public static ThemeStyleSheet StyleSheetDark { get; }
+        public static ThemeStyleSheet StyleSheetThemeLight { get; }
+        public static ThemeStyleSheet StyleSheetThemeDark { get; }
+        public static ThemeStyleSheet CurrentStyleSheetTheme { get { return EditorGUIUtility.isProSkin ? StyleSheetThemeDark : StyleSheetThemeLight; } }
 
         static UIToolkitEditorUtility()
         {
-            StyleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Packages/com.ugf.editortools/Data.Editor/UGF.EditorTools.Editor.Styles.uss") ?? throw new ArgumentException("UIToolkit Styles not found.");
-            StyleSheetLight = AssetDatabase.LoadAssetAtPath<ThemeStyleSheet>("Packages/com.ugf.editortools/Data.Editor/UGF.EditorTools.Editor.Styles.Light.tss") ?? throw new ArgumentException("UIToolkit Styles not found.");
-            StyleSheetDark = AssetDatabase.LoadAssetAtPath<ThemeStyleSheet>("Packages/com.ugf.editortools/Data.Editor/UGF.EditorTools.Editor.Styles.Dark.tss") ?? throw new ArgumentException("UIToolkit Styles not found.");
+            StyleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>(StyleSheetPath) ?? throw new ArgumentException("UIToolkit Styles not found.");
+            StyleSheetThemeLight = AssetDatabase.LoadAssetAtPath<ThemeStyleSheet>(StyleSheetThemeLightPath) ?? throw new ArgumentException("UIToolkit Styles not found.");
+            StyleSheetThemeDark = AssetDatabase.LoadAssetAtPath<ThemeStyleSheet>(StyleSheetThemeDarkPath) ?? throw new ArgumentException("UIToolkit Styles not found.");
+        }
+
+        public static void AddStyleSheets(VisualElement visualElement)
+        {
+            if (visualElement == null) throw new ArgumentNullException(nameof(visualElement));
+
+            visualElement.styleSheets.Add(StyleSheet);
+            visualElement.styleSheets.Add(CurrentStyleSheetTheme);
         }
 
         public static void AddFieldClasses<T>(BaseField<T> field)
