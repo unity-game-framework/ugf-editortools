0225097396fc28d1ec6efb8a4a72b05373127edc
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2024-07-07 12:47:20 +0300

    Add IconButtonElement

---
+++ Packages/com.ugf.editortools/Editor/UIToolkit.Elements/IconButtonElement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine.UIElements;
+
+namespace UGF.EditorTools.Editor.UIToolkit.Elements
+{
+    public class IconButtonElement : Button
+    {
+        public static string UssClassName { get; } = "ugf-icon-button";
+
+        public IconButtonElement()
+        {
+            AddToClassList(UssClassName);
+
+            UIToolkitEditorUtility.AddStyleSheets(this);
+        }
+    }
+}
--- Packages/com.ugf.editortools/Editor/UIToolkit.Elements/ObjectFieldLinkedElement.cs
+++ Packages/com.ugf.editortools/Editor/UIToolkit.Elements/ObjectFieldLinkedElement.cs
@@ -5,19 +5,20 @@ namespace UGF.EditorTools.Editor.UIToolkit.Elements
 {
     public class ObjectFieldLinkedElement : ObjectField
     {
-        public Button LinkButtonElement { get; }
+        public IconButtonElement LinkButtonElement { get; }
         public static string LinkButtonUssClassName { get; } = "ugf-object-field-linked__button";
 
         public ObjectFieldLinkedElement()
         {
-            LinkButtonElement = new Button();
+            LinkButtonElement = new IconButtonElement();
             LinkButtonElement.AddToClassList(LinkButtonUssClassName);
 
             VisualElement inputElement = this.Query<VisualElement>(className: inputUssClassName).First();
             VisualElement selectorElement = inputElement.Query<VisualElement>(className: selectorUssClassName).First();
 
             inputElement.Insert(inputElement.IndexOf(selectorElement), LinkButtonElement);
-            styleSheets.Add(UIToolkitEditorUtility.StyleSheet);
+
+            UIToolkitEditorUtility.AddStyleSheets(this);
         }
     }
 }
