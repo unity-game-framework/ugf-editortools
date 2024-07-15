84ea43fe80f52e9c8a43929b45996e9985a97e18
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2024-07-15 16:18:05 +0300

    Add DropAreaElement

---
+++ Packages/com.ugf.editortools/Editor/IMGUI/DropAreaElement.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine.UIElements;
+
+namespace UGF.EditorTools.Editor.IMGUI
+{
+    public class DropAreaElement : HelpBox
+    {
+        public DropAreaManipulator Manipulator { get; }
+
+        public DropAreaElement(Type acceptType)
+        {
+            Manipulator = new DropAreaManipulator(this, acceptType);
+
+            text = "Drag and drop assets here.";
+            style.height = EditorGUIUtility.singleLineHeight * 3F;
+            style.justifyContent = Justify.Center;
+
+            Insert(0, new Image
+            {
+                image = AssetPreview.GetMiniTypeThumbnail(acceptType),
+                style =
+                {
+                    width = EditorGUIUtility.singleLineHeight,
+                    height = EditorGUIUtility.singleLineHeight,
+                    marginRight = EditorGUIUtility.standardVerticalSpacing
+                }
+            });
+        }
+    }
+}
